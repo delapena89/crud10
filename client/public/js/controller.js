2492ffd6a1cdcc14f2ee367c57e23e02b9968f70
Luis <delapenl@gmail.com>
2015-09-29 11:01:46 -0600

    on submit beers appear on the DOM

--- client/public/js/controller.js
+++ client/public/js/controller.js
@@ -1,3 +1,23 @@
-app.controller('BeerController', function($scope, httpFactory) {
+app.controller('BeerController', function($scope, httpFactory, $http) {
+  $scope.beer = {};
+  getBeers = function(url) {
+    console.log(url);
+    httpFactory.getAll(url)
+    .then(function(response) {
+      $scope.beers = response.data;
+    });
+  };
 
-});
+  getBeers('api/v1/beers');
+
+  $scope.postBeer = function() {
+    var payload = $scope.beer;
+    httpFactory.post('api/v1/beers', payload)
+    .then(function(response){
+      $scope.beers.push(response.data);
+      console.log($scope.beers);
+      $scope.beer = {};
+    });
+  };
+
+}); //close controller
