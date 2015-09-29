dfaf87349e2aef92a622380fe9563b7e157170b8
Luis <delapenl@gmail.com>
2015-09-29 14:04:21 -0600

    started spliting project into columns

--- client/public/js/controller.js
+++ client/public/js/controller.js
@@ -1,23 +1,35 @@
 app.controller('BeerController', function($scope, httpFactory, $http) {
   $scope.beer = {};
   getBeers = function(url) {
-    console.log(url);
     httpFactory.getAll(url)
     .then(function(response) {
       $scope.beers = response.data;
-    });
-  };
+  });
+};
+getBeers('api/v1/beers');
 
-  getBeers('api/v1/beers');
 
-  $scope.postBeer = function() {
-    var payload = $scope.beer;
-    httpFactory.post('api/v1/beers', payload)
-    .then(function(response){
-      $scope.beers.push(response.data);
-      console.log($scope.beers);
-      $scope.beer = {};
-    });
-  };
+$scope.postBeer = function() {
+  var payload = $scope.beer;
+  httpFactory.post('/api/v1/beers', payload)
+  .then(function(response) {
+    $scope.beers.push(response.data);
+    $scope.beer = {};
+    getBeers('api/v1/beers');
+  });
+};
+
+$scope.deleteBeer = function(id) {
+  httpFactory.delete('api/v1/beer/' + id)
+  .then(function(response) {
+    getBeers('api/v1/beers');
+  });
+};
+
+
+
+
+
+
 
 }); //close controller
